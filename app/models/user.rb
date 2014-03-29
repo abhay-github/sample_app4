842f0aac790bae89c1561838e192783ce38534e8
Abhay Agrawal <abhayemailid@gmail.com>
2014-03-29 16:07:55 +0530

    modification to replies

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,14 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
 
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	VALID_USERNAME_REGEX = /\A[\w+\-.]+\z/
+
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { without: /\s/,
-     			message: "cannot contain whitespaces" }
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_USERNAME_REGEX,
+     			message: "can only contain letters, numbers, or any of these: underscore, hyphen, dot" },
+     			length: {minimum: 3}
 
  	has_secure_password
 
